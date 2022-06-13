0b1fd9802bed112669e03e43fb6610772a9f2380
geepee500 <62014136+geepee500@users.noreply.github.com>
2022-06-13 12:28:07 +0100

    first commit

---
+++ js/custom.js
@@ -0,0 +1,48 @@
+$(document).ready(function() {
+ 
+  var owl = $("#owl-demo");
+ 
+  owl.owlCarousel({
+     
+      itemsCustom : [
+        [0, 2],
+        [450, 3],
+        [600, 3],
+        [700, 3],
+        [1000, 4],
+        [1200, 4],
+        [1400, 4],
+        [1600, 4]
+      ],
+      navigation : true
+ 
+  });
+ 
+});
+
+$(document).ready(function() {
+  $(".testimonial-carousel").slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: false,
+    arrows: true,
+    prevArrow: $(".testimonial-carousel-controls .prev"),
+    nextArrow: $(".testimonial-carousel-controls .next")
+  });
+});
+
+
+window.onscroll = function() {myFunctioned()};
+
+var header = document.getElementById("navigation");
+var sticky = header.offsetTop;
+
+function myFunctioned() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
+
